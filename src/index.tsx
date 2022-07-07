24fcd3a551943694b1ae5454e116abebf9f93279
Petter Sellesbakk Bjørkås <pbjorkaas@enua.no>
2022-07-07 20:48:56 +0200

    add localization support

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import './i18n';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +10,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback="Spinner..">
+      <App />
+    </Suspense>
   </React.StrictMode>
 );
 
