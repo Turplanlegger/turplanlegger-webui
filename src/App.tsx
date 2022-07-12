5e49cb7003852c2c5a19ca510f26cedf28e7b370
Petter <43450467+peseb@users.noreply.github.com>
2022-07-12 20:18:46 +0200

    Add internationalization (#3)
    * add localization support
    
    * Edit failing test
    
    * add .env files
    
    * remove console.log
    
    * get configvalue from json

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Box, styled } from "@mui/material";
+import { useTranslation } from "react-i18next";
 import {
   BrowserRouter,
   Routes,
@@ -6,7 +7,7 @@ import {
 } from "react-router-dom";
 import './App.css';
 import { Home } from "./pages/Home";
-import { Opprett } from "./pages/Opprett";
+import { Create } from "./pages/Create";
 
 const FullScreen = styled(Box)({
   height: '100vh',
@@ -18,12 +19,14 @@ const FullScreen = styled(Box)({
 })
 
 export const App = () => {
+  const { t } = useTranslation();
+  
   return (
     <FullScreen>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/opprett" element={<Opprett />} />
+          <Route path={`/${t('app.routes.create')}`} element={<Create />} />
         </Routes>
     </BrowserRouter>
   </FullScreen>
