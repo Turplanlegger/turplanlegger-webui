85fdcbee1d5470d3ab45a6b6b0ea8b999275fee2
Are Schjetne <sixcare.as@gmail.com>
2022-09-27 20:38:52 +0200

    Feature/mainpage (#6)
    Main layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,28 @@
-import { Typography } from '@mui/material';
+import { Box, Button, Grid, Typography } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
 
 export const Home = () => {
+  const navigate = useNavigate();
+  const { t } = useTranslation();
+  document.title = t('home.title');
+
   return (
     <>
-      <Typography variant="h1" fontSize={50}>
-        Velkommen til Turplanleggeren
-      </Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Box>
+            <Typography variant="h2">{t('home.welcome')}</Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={12}>
+          <Box>
+            <Button color="primary" variant="contained" onClick={() => navigate('/route/create')}>
+              {t('common.create')} {t('trip.trip')}
+            </Button>
+          </Box>
+        </Grid>
+      </Grid>
     </>
   );
 };
