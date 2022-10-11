0b4115659434ac5a57f9df23ffcbe51872c40267
Are Schjetne <sixcare.as@gmail.com>
2022-10-11 20:17:18 +0200

    Added create list call

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -4,29 +4,41 @@ export class Api {
   url: string;
   token: string;
 
-  constructor() {
+  constructor(token: string) {
     this.url = config.api_base_url;
-    this.token = 'test';
+    this.token = token; // add getToken() here!
   }
 
-  async call_get(path: string) {
+  async get(path: string) {
     const response = await fetch(this.url + path, {
       method: 'GET',
-      headers: { Authorization: `Api-Key ${this.token}` }
+      headers: { Authorization: `Bearer ${this.token}` }
     });
     return await response.json();
   }
 
-  async call_post(path: string, data: JSON) {
+  async post(path: string, data: object) {
     const response = await fetch(this.url + path, {
       method: 'POST',
-      headers: { Authorization: `Api-Key ${this.token}` },
+      headers: {
+        'Content-Type': 'Application/json',
+        Authorization: `Bearer ${this.token}`
+      },
       body: JSON.stringify(data)
     });
     return await response.json();
   }
 
   async get_list(id: number) {
-    return await this.call_get(`/item_list/${id}`);
+    return await this.get(`/item_list/${id}`);
+  }
+
+  async create_list(type: string, name?: string, items?: string[], itemsChecked?: string[]) {
+    return await this.post('/item_list', {
+      name: name,
+      type: type,
+      items: items,
+      items_checked: itemsChecked
+    });
   }
 }
