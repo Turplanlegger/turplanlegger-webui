51e709fdbd64b72a688517c38206bdb6c99cca74
Petter <peseb@icloud.com>
2022-11-01 19:54:22 +0100

    log trips

--- src/pages/Trips.tsx
+++ src/pages/Trips.tsx
@@ -5,5 +5,6 @@ import { myTrips } from '../state/tripState';
 export const Trips = () => {
   const tripsLoadable = useRecoilValueLoadable(myTrips);
   const trips = tripsLoadable.state === 'hasValue' ? tripsLoadable.contents : [];
+  console.log(trips);
   return <Typography>Trips</Typography>;
 };
