86cc16ad5b017ec8299f6b6117a43f91a0ff63e5
Are Schjetne <sixcare.as@gmail.com>
2022-11-08 20:38:50 +0100

    WIP

--- src/components/NotesList.tsx
+++ src/components/NotesList.tsx
@@ -1,14 +1,18 @@
 import {
   Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Collapse,
   Divider,
   Drawer,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-  Toolbar
+  Grid,
+  TextField,
+  Toolbar,
+  Typography
 } from '@mui/material';
+import React from 'react';
 
 interface Props {
   notes: Note[];
@@ -21,15 +25,45 @@ type Note = {
 };
 
 export const NoteList = ({ notes }: Props) => {
+  const [checked, setChecked] = React.useState(false);
+
+  const handleChange = () => {
+    setChecked((prev) => !prev);
+  };
+
   const noteList = (
-    <List>
+    <Grid container spacing={4}>
       {notes.map((item, index) => (
-        <ListItem key={index} disablePadding>
-          <>{console.debug(item)}</>
-          <ListItemText>{item.name}</ListItemText>
-        </ListItem>
+        <Grid item key={index} xs={12} sm={6} md={4}>
+          <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+            <CardContent sx={{ flexGrow: 1 }}>
+              <Typography gutterBottom variant="h5" component="h3">
+                {item.name}
+              </Typography>
+              <Collapse in={checked} collapsedSize={77}>
+                <TextField
+                  multiline
+                  fullWidth
+                  value={item.content}
+                  variant="standard"
+                  id="fullWidth"
+                  // onChange={}
+                  InputProps={{
+                    disableUnderline: true
+                  }}
+                />
+              </Collapse>
+              <CardActions>
+                <Button size="small" onClick={handleChange}>
+                  Show all
+                </Button>
+                <Button size="small">save</Button>
+              </CardActions>
+            </CardContent>
+          </Card>
+        </Grid>
       ))}
-    </List>
+    </Grid>
   );
   return noteList;
 };
--- src/pages/Notes.tsx
+++ src/pages/Notes.tsx
@@ -1,15 +1,17 @@
 import { Typography } from '@mui/material';
+import { useTranslation } from 'react-i18next';
 import { useRecoilValueLoadable } from 'recoil';
 import { myNotes } from '../state/noteState';
 import { NoteList } from '../components/NotesList';
 
 export const Notes = () => {
+  const { t } = useTranslation();
   const notesLoadable = useRecoilValueLoadable(myNotes);
   const notes = notesLoadable.state === 'hasValue' ? notesLoadable.contents : undefined;
   console.debug(notes);
   return (
     <>
-      <Typography>Trips</Typography>
+      <Typography variant="h2">{t('note.my_notes')}</Typography>
       {notes && <NoteList notes={notes.note}></NoteList>}
     </>
   );
