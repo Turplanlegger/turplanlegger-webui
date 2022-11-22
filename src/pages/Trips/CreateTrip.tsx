b1142a4d35266ce5119214a93ff5b73aa02522a4
Petter <peseb@icloud.com>
2022-11-22 20:22:02 +0100

    Change Box to Trip

--- src/pages/Trips/CreateTrip.tsx
+++ src/pages/Trips/CreateTrip.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, FormControlLabel, Switch, TextField, Typography } from '@mui/material';
+import { Box, Button, FormControlLabel, Grid, Switch, TextField, Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
@@ -12,41 +12,45 @@ export const CreateTrip = () => {
   const [publicTrip, setPublicTrip] = useState(true);
   return (
     <Box height={'100%'} margin={5}>
-      <Typography style={{ marginTop: 20 }}>{t('trip.new_trip')}</Typography>
-      <Box
-        style={{
-          display: 'flex',
-          height: '100%',
-          flexDirection: 'column',
-          justifyContent: 'flex-end'
-        }}>
-        <TextField id="outlined-basic" label="Trip name" variant="outlined" />
-        <LocalizationProvider dateAdapter={AdapterMoment}>
-          <DatePicker
-            label="Start time"
-            value={startTime}
-            onChange={(newValue) => {
-              setStartTime(newValue);
-            }}
-            renderInput={(params) => <TextField {...params} />}
-          />
-          <DatePicker
-            label="End time"
-            value={endTime}
-            onChange={(newValue) => {
-              setEndTime(newValue);
-            }}
-            renderInput={(params) => <TextField {...params} />}
-          />
-          <FormControlLabel
-            control={<Switch defaultChecked onChange={() => setPublicTrip(!publicTrip)} />}
-            label={publicTrip ? 'Public' : 'Private'}
-          />
-        </LocalizationProvider>
-        <Box display={'flex'} marginBottom={10} marginTop={10}>
-          <Button fullWidth={true}>Cancel</Button>
-          <Button fullWidth={true}>Create</Button>
-        </Box>
+      <LocalizationProvider dateAdapter={AdapterMoment}>
+        <Grid container direction="column" justifyContent="center" spacing={2}>
+          <Grid item>
+            <Typography style={{ marginTop: 20 }}>{t('trip.new_trip')}</Typography>
+          </Grid>
+          <Grid item>
+            <TextField id="outlined-basic" label="Trip name" variant="outlined" />
+          </Grid>
+          <Grid item>
+            <DatePicker
+              label="Start time"
+              value={startTime}
+              onChange={(newValue) => {
+                setStartTime(newValue);
+              }}
+              renderInput={(params) => <TextField {...params} />}
+            />
+          </Grid>
+          <Grid item>
+            <DatePicker
+              label="End time"
+              value={endTime}
+              onChange={(newValue) => {
+                setEndTime(newValue);
+              }}
+              renderInput={(params) => <TextField {...params} />}
+            />
+          </Grid>
+          <Grid item>
+            <FormControlLabel
+              control={<Switch defaultChecked onChange={() => setPublicTrip(!publicTrip)} />}
+              label={publicTrip ? 'Public' : 'Private'}
+            />
+          </Grid>
+        </Grid>
+      </LocalizationProvider>
+      <Box display={'flex'} marginBottom={10} marginTop={10}>
+        <Button fullWidth={true}>Cancel</Button>
+        <Button fullWidth={true}>Create</Button>
       </Box>
     </Box>
   );
