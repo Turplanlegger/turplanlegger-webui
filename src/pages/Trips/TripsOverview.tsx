39fa87081859be26466bf2b9cbc75a3dbf312fc6
Petter <43450467+peseb@users.noreply.github.com>
2023-01-10 18:29:01 +0100

    Feature: Create trip (#14)
    * start on create trip
    
    * create trip
    
    * add fields
    
    * Change Box to Trip
    
    * Fix style og create trip modal
    
    * add components and set margin top for topbar
    
    * create and delete trip works
    
    * add language support
    
    * small cleanup
    
    * cleanup
    
    * fix build
    
    * correct translation

---
+++ src/pages/Trips/TripsOverview.tsx
@@ -0,0 +1,20 @@
+import { Grid } from '@mui/material';
+import { Trip } from '../../state/tripState';
+import { TripInfo } from './TripInfo';
+
+interface Props {
+  trips: Trip[];
+}
+
+export const TripsOverview = ({ trips }: Props) => {
+  return (
+    <Grid container spacing={2} direction="row" sx={{ margin: 1 }}>
+      {trips &&
+        trips.map((trip) => (
+          <Grid item key={trip.name}>
+            <TripInfo key={trip.name} trip={trip} />
+          </Grid>
+        ))}
+    </Grid>
+  );
+};
