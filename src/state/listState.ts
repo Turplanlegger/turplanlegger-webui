63a8789df234b60c5e0e7fc58c5a8de77a57212e
Are Schjetne <sixcare.as@gmail.com>
2023-01-28 15:48:21 +0100

    Corrected vars

--- src/state/listState.ts
+++ src/state/listState.ts
@@ -2,16 +2,16 @@ import { atom, selector } from 'recoil';
 import { ItemList } from '../models/Types';
 import { apiState } from './apiState';
 
-export const initializeNotesSelector = selector<ItemList[]>({
+export const initializeItemListsSelector = selector<ItemList[]>({
   key: `initializeItemListsSelector`,
   get: async ({ get }) => {
     const api = get(apiState);
     const result = await api?.get('/item_list/mine');
-    return result?.status === 'ok' ? result.note : [];
+    return result?.status === 'ok' ? result.item_list : [];
   }
 });
 
 export const itemListState = atom<ItemList[]>({
   key: 'itemListState',
-  default: initializeNotesSelector
+  default: initializeItemListsSelector
 });
