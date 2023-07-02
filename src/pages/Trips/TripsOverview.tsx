6f29910f901d385348d7d70792a56e5bc6ad271d
Are Schjetne <sixcare.as@gmail.com>
2023-07-02 12:56:24 +0200

    Changed key

--- src/pages/Trips/TripsOverview.tsx
+++ src/pages/Trips/TripsOverview.tsx
@@ -11,8 +11,8 @@ export const TripsOverview = ({ trips }: Props) => {
     <Grid container spacing={2} direction="row" sx={{ margin: 1 }}>
       {trips &&
         trips.map((trip) => (
-          <Grid item key={trip.name} xs={8} md={4}>
-            <TripInfo key={trip.name} trip={trip} />
+          <Grid item key={trip.id} xs={8} md={4}>
+            <TripInfo key={trip.id} trip={trip} />
           </Grid>
         ))}
     </Grid>
