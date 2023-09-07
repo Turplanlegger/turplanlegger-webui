d43161676d86b7e9b11c9674b56725a64f23b4fe
Martin Lindelien Høgli <6975348+crazycatman@users.noreply.github.com>
2023-09-07 20:16:23 +0200

    Changed from hardcoded value to locale

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,18 +1,20 @@
 import { Box, Typography } from '@mui/material';
 import { useRecoilValue } from 'recoil';
+import { useTranslationWrapper } from 'services/Translation';
 import { whoamiState } from 'state/userState';
 
 export const Profile = () => {
   const user = useRecoilValue(whoamiState);
+  const t = useTranslationWrapper();
 
   return Object.keys(user).length === 0 ? (
     <Box>
-      <Typography variant="h2">Profile</Typography>
+      <Typography variant="h2">{t('profile.profile')}</Typography>
       <Typography variant="h3"> U did boo-boo </Typography>
     </Box>
   ) : (
     <Box>
-      <Typography variant="h2">Profile</Typography>
+      <Typography variant="h2">{t('profile.profile')}</Typography>
       <Typography variant="h3"> {user.name} </Typography>
     </Box>
   );
