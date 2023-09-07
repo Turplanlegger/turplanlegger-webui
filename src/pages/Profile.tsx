ab3e74f5dbd6f24181be84d8c9532b2a27f5764b
Martin Lindelien Høgli <6975348+crazycatman@users.noreply.github.com>
2023-09-07 20:11:30 +0200

    Returns user name

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,5 +1,19 @@
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
+import { useRecoilValue } from 'recoil';
+import { whoamiState } from 'state/userState';
 
 export const Profile = () => {
-  return <Typography>Profile</Typography>;
+  const user = useRecoilValue(whoamiState);
+
+  return Object.keys(user).length === 0 ? (
+    <Box>
+      <Typography variant="h2">Profile</Typography>
+      <Typography variant="h3"> U did boo-boo </Typography>
+    </Box>
+  ) : (
+    <Box>
+      <Typography variant="h2">Profile</Typography>
+      <Typography variant="h3"> {user.name} </Typography>
+    </Box>
+  );
 };
