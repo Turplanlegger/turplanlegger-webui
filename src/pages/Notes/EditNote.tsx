d8d5b1fac95ef04064d2ff58293f4f8e0499da97
Are Schjetne <sixcare.as@gmail.com>
2023-09-09 14:09:21 +0200

    WIP: update note example

--- src/pages/Notes/EditNote.tsx
+++ src/pages/Notes/EditNote.tsx
@@ -2,6 +2,11 @@ import { Button, Stack, Switch, TextField, Typography } from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2';
 import { useTranslationWrapper } from 'services/Translation';
 import { Note } from '../../models/Types';
+import { editModalOpen } from 'components/CustomModal/modalState';
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+import { useState } from 'react';
+import { apiState } from 'state/apiState';
+import { noteState } from 'state/noteState';
 
 interface Props {
   note: Note;
@@ -9,6 +14,39 @@ interface Props {
 
 export const EditNote = ({ note }: Props) => {
   const t = useTranslationWrapper();
+  const api = useRecoilValue(apiState);
+  const setOpen = useSetRecoilState(editModalOpen);
+  const [name, setName] = useState<string>(note.name);
+  const [content, setContent] = useState<string>(note.content);
+  const [privacy, setPrivacyNote] = useState(note.private);
+  const [notes, setNotes] = useRecoilState(noteState);
+
+  const updateNote = async () => {
+    if (name === note.name && content === note.content && note.private == privacy) {
+      console.warn('No fields updated');
+      return false;
+    }
+
+    const updatedNote = {
+      name: name,
+      content: content,
+      private: privacy
+    } as Note;
+
+    console.log(notes, setNotes);
+
+    const result = await api
+      ?.put(`/note/${note.id}`, updatedNote) // This does not yet exist in the API
+      .then((response) => {
+        console.debug('Ok?');
+        console.debug(response.status, response.ok);
+      })
+      .catch((response) => {
+        console.error('Not ok!');
+        console.debug(response.status, response.ok);
+      });
+    console.debug(result);
+  };
 
   return (
     <Grid container direction="column" spacing={2}>
@@ -17,8 +55,8 @@ export const EditNote = ({ note }: Props) => {
           id="outlined-basic"
           label={t('common.name')}
           variant="outlined"
-          value={note.name}
-          onChange={(e) => console.debug(e?.target.value)}
+          value={name}
+          onChange={(e) => setName(e?.target.value)}
         />
       </Grid>
       <Grid>
@@ -29,25 +67,25 @@ export const EditNote = ({ note }: Props) => {
           id="outlined-basic"
           label={t('common.content')}
           variant="outlined"
-          value={note.content}
-          onChange={(e) => console.debug(e?.target.value)}
+          value={content}
+          onChange={(e) => setContent(e?.target.value)}
         />
       </Grid>
       <Grid>
         <Stack direction="row" spacing={1} alignItems="center">
           <Typography>{t('common.private')}</Typography>
-          <Switch value={note.private} onChange={() => console.debug(!note.private)} />
+          <Switch value={privacy} onChange={() => setPrivacyNote(!privacy)} />
           <Typography>{t('common.public')}</Typography>
         </Stack>
       </Grid>
       <Grid container justifyContent="flex-end">
         <Grid>
-          <Button variant="outlined" color="warning" onClick={() => console.debug(false)}>
+          <Button variant="outlined" color="warning" onClick={() => setOpen(false)}>
             {t('common.cancel')}
           </Button>
         </Grid>
         <Grid>
-          <Button variant="contained" color="success" onClick={() => console.debug('yolo')}>
+          <Button variant="contained" color="success" onClick={() => updateNote()}>
             {t('common.save')}
           </Button>
         </Grid>
