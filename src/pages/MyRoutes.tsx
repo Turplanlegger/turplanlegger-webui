d53e54f6dd1fcf2cc3ce5eeefa9d9d609a179ccb
Petter Bjørkås <peseb@icloud.com>
2025-02-18 20:50:46 +0100

    wip

--- src/pages/MyRoutes.tsx
+++ src/pages/MyRoutes.tsx
@@ -1,5 +1,6 @@
 import L from 'leaflet';
 import { useEffect, useRef } from 'react';
+import iconSvg from "../Map/marker.svg";
 
 export const MyRoutes = () => {
   const map = useRef<L.Map | undefined>();
@@ -7,19 +8,32 @@ export const MyRoutes = () => {
   useEffect(() => {
     if (!map.current) {
       map.current = L.map('map').setView([64.505, 10.09], 5);
-      if (map) {
-        const id = 'topo';
-        const layer = L.tileLayer(`https://cache{s}.kartverket.no/v1/wmts/1.0.0/${id}/default/webmercator/{z}/{y}/{x}.png`,
-          {
-              // format: 'image/png',
-              minZoom: 5,
-              maxZoom: 18,
-              detectRetina: true,
-              attribution: '<a href="https://www.kartverket.no/">Kartverket</a>',
-              subdomains: ['', '2', '3', '4']
-          });
-        layer.addTo(map.current);
-      }
+      const id = 'topo';
+      const layer = L.tileLayer(`https://cache{s}.kartverket.no/v1/wmts/1.0.0/${id}/default/webmercator/{z}/{y}/{x}.png`,
+        {
+            // format: 'image/png',
+            minZoom: 5,
+            maxZoom: 18,
+            detectRetina: true,
+            attribution: '<a href="https://www.kartverket.no/">Kartverket</a>',
+            subdomains: ['', '2', '3', '4']
+        });
+      layer.addTo(map.current);
+
+      map.current.on('click', function(e) {        
+        var popLocation= e.latlng;
+        // L.popup()
+        //   .setLatLng(popLocation)
+        //   .setContent('<p>Hello world!<br />This is a nice popup.</p>')
+        //   .openOn(map.current!);
+          const icon = L.icon({
+            className: "marker",
+            iconUrl: iconSvg,
+            iconSize: [50, 50],
+            iconAnchor: [25, 25]
+        });
+        L.marker(popLocation, {icon: icon}).addTo(map.current!)
+    });
     }
   }, [])
 
