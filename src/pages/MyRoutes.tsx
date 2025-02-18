7b8ef1d93ad7b96e2856228c1515ddad8e8611ac
Petter Bjørkås <peseb@icloud.com>
2025-02-18 20:02:51 +0100

    Add map

--- src/pages/MyRoutes.tsx
+++ src/pages/MyRoutes.tsx
@@ -1,5 +1,22 @@
-import { Typography } from '@mui/material';
+import L from 'leaflet';
+import { useEffect, useRef } from 'react';
 
 export const MyRoutes = () => {
-  return <Typography>MyRoutes</Typography>;
+  const map = useRef<L.Map | undefined>();
+  
+  useEffect(() => {
+    if (!map.current) {
+      map.current = L.map('map').setView([64.505, 10.09], 5);
+      if (map) {
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          maxZoom: 19,
+          attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map.current);
+      }
+    }
+  }, [])
+
+  
+
+  return <div id="map" style={{ height: '100vh' }}></div>;
 };
