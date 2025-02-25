a63fca7eab121f304f19a98ff3872526b9f8dda5
Petter Bjørkås <peseb@icloud.com>
2025-02-25 21:22:52 +0100

    Add possibility to end editing

--- src/pages/Routes/TurplanleggerMap.tsx
+++ src/pages/Routes/TurplanleggerMap.tsx
@@ -7,6 +7,7 @@ interface Props {
 
 export const TurplanleggerMap = ({setDistance}: Props) => {
   const [points, setPoints] = useState<L.LatLng[]>([]);
+  const [editing, setEditing] = useState(true);
   const [layer] = useState(L.layerGroup());
   const [pathLayer] = useState(L.layerGroup());
   const map = useRef<L.Map | undefined>();
@@ -25,26 +26,49 @@ export const TurplanleggerMap = ({setDistance}: Props) => {
         });
       layer.addTo(map.current);
 
-      map.current.on('click', function(e) {        
-        var popLocation= e.latlng;
-        setPoints(points => [...points, popLocation])
-      });
+      
 
       
     }
   }, [])
 
   useEffect(() => {
+    map.current?.removeEventListener('click')
+    map.current!.on('click', function(e) {        
+      if (!editing) return;
+      var popLocation= e.latlng;
+      if (points.length > 0) {
+        const start = popLocation;
+        const end = points[points.length - 1]
+        const distanceInMeters = start.distanceTo(end);
+        // Disable editing if last click was less than 10meters away from prior point
+        if (distanceInMeters < 10) {
+          setEditing(false);
+          return;
+        }
+      }
+
+      setPoints(points => [...points, popLocation])
+    });
+  }, [points, editing])
+
+  useEffect(() => {
+    map.current?.removeEventListener('mousemove')
     map.current!.on('mousemove', function(e) {        
       var popLocation= e.latlng;
       if (points.length > 0) {
         pathLayer.clearLayers()
+
+        if (!editing) {
+          return;
+        }
+
         const lastPoint = points[points.length - 1];
         L.polyline([lastPoint, popLocation]).addTo(pathLayer);
         pathLayer.addTo(map.current!)
       }
     });
-  }, [points])
+  }, [points, editing])
 
   useEffect(() => {
     layer.clearLayers();
@@ -52,7 +76,7 @@ export const TurplanleggerMap = ({setDistance}: Props) => {
     // Markers
     points.map(point => {
       L.marker(point)
-      .addTo(layer)})
+      .addTo(layer).on('click', () => {setEditing(false)});})
       
       // Line
       const coordinates = points.map(p => L.latLng([p.lat, p.lng]));
